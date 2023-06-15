a18458a5c2dfdf7a4e4d49d693e32f5637fe52b9
Inheritx-Flutter <inheritx-flutter@Dharmeshs-Mac-mini.local>
2023-06-15 10:31:04 +0530

    Initially set a whatsapp package code for ios app

---
+++ ios/Classes/WhatsappSharePlugin.m
@@ -0,0 +1,8 @@
+#import "WhatsappSharePlugin.h"
+#import <whatsapp_share/whatsapp_share-Swift.h>
+
+@implementation WhatsappShare
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftWhatsappShare registerWithRegistrar:registrar];
+}
+@end
